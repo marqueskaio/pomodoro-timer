bd313e10f045cb8f1eb87aaa3ca14d4ab059392f
kaio.marques <marques.kaio16@gmail.com>
2023-05-28 19:51:16 -0300

    timer and button components

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useInterval } from "../hooks/use-interval";
+import { Button } from "./button";
+import { Timer } from "./timer";
 
 interface Props {
     defaultPomodoroTime: number;
@@ -12,5 +14,11 @@ export function PomodoroTimer(props: Props): JSX.Element {
         setMainTime(mainTime - 1)
     }, 1000)
 
-    return <div>Olá Mundo {mainTime} !</div>
+    return (
+        <div className="pomodoro">
+            <h2>You are: working</h2>
+            <Timer mainTime={mainTime} />
+            <Button text="teste" onClick={() => console.log(1)}></Button>
+        </div>
+    )
 }
